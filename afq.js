19d56a977403c77c8641201d79bc00d0cb0e9d1c
Mikael Karon <mikael@karon.se>
2016-08-04 11:33:52 +0200

    Unborke script order by adding a provide method

--- afq.js
+++ afq.js
@@ -1,27 +1,25 @@
-(function(afq, x, w, s, t, q, m) {
-  if (w[afq]) {
-    Object.keys(w[afq]).forEach(function(k) {
-      if (t.call(this[k]) === "[object Function]") {
-        m[k] = this[k];
-      }
-    }, w[afq]);
-
-    if (w[afq].q) {
-      q = q.concat(w[afq].q);
-    }
-  }
+(function(afq, x, w, s, t, q) {
+  q = w[afq] && w[afq].q ? q.concat(w[afq].q) : q;
 
   afq = afq === w[x]
     ? w[afq]
-    : w[w[x] = afq] = function(n) {
-      afq[n].apply(this, s.call(arguments, 1));
-    };
+    : w[w[x] = afq] = (function(_q) {
+      function _afq(n) {
+        _afq[n] ? _afq[n].apply(this, s.call(arguments, 1)) : (_q[n] = _q[n] || []).push(arguments);
+      }
 
-  Object.keys(m).forEach(function(k) {
-    afq[k] = this[k];
-  }, m);
+      _afq.provide = function(n, f) {
+        _afq[n] = f;
+
+        _q[n] && _q[n].forEach(function(a) {
+          f.apply(this, a);
+        }, this);
+      };
+
+      return _afq;
+    })({});
 
   while (q.length) {
     afq.apply(afq, q.shift());
   }
-})("afq", "afqObject", window, Array.prototype.slice, Object.prototype.toString, [], {});
+})("afq", "afqObject", window, Array.prototype.slice, Object.prototype.toString, []);
