ab99c6e942f1bc6a739be915359980a1898c7309
Mikael Karon <mikael@karon.se>
2016-11-03 13:17:32 +0100

    UMD and packaging

--- afq.js
+++ afq.js
@@ -1,17 +1,30 @@
-(function(afq, x, w, s, t, q) {
-  q = w[afq] && w[afq].q ? q.concat(w[afq].q) : q;
+(function (modules, root, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(modules, factory);
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = factory.call(root);
+  } else {
+    root["mu-afq/afq"] = factory.call(root);
+  }
+})([], this, function () {
+  var afq = "afq";
+  var x = "afqObject";
+  var s = Array.prototype.slice;
+  var t = Object.prototype.toString;
+  var w = this;
+  var q = w[afq] && w[afq].q ? s.call(w[afq].q) : [];
 
   afq = afq === w[x]
     ? w[afq]
-    : w[w[x] = afq] = (function(_q) {
+    : w[w[x] = afq] = (function (_q) {
       function _afq(n) {
         _afq[n] ? _afq[n].apply(this, s.call(arguments, 1)) : (_q[n] = _q[n] || []).push(s.call(arguments, 1));
       }
 
-      _afq.provide = function(n, f) {
+      _afq.provide = function (n, f) {
         _afq[n] = f;
 
-        _q[n] && _q[n].forEach(function(a) {
+        _q[n] && _q[n].forEach(function (a) {
           f.apply(this, a);
         }, this);
       };
@@ -22,4 +35,6 @@
   while (q.length) {
     afq.apply(afq, q.shift());
   }
-})("afq", "afqObject", window, Array.prototype.slice, Object.prototype.toString, []);
+
+  return afq;
+});
